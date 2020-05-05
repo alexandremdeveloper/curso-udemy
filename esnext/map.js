db379916c9c921c539020c3f609d12c68ef6cd47
alexandremdeveloper <alexandresmartinsbr@gmail.com>
2020-05-05 15:41:31 -0300

    promises

---
+++ esnext/map.js
@@ -0,0 +1,16 @@
+const tecnologias = new Map()
+tecnologias.set('react', {framework: false})
+tecnologias.set('angular', {framework: true})
+
+console.log(tecnologias.react)
+console.log(tecnologias.get('react').framework)
+
+const chavesVariadas = new Map([
+    [function () { }, 'Função'],
+    [{}, 'Objeto'],
+    [123, 'Número'],
+])
+
+chavesVariadas.forEach(( vl, ch) => {
+    console.log(chavesVariadas.delete(123))
+})
